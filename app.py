f09969c58660dc3ae94a55d24123fafc3b73a4e1
blueapple16 <76197636+blueapple16@users.noreply.github.com>
2021-07-29 10:22:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -164,8 +164,8 @@ st.write(fig2)
 
 # ### Technical Analysis of the stock
 # ### install, for streamlit share
-import os
-import sys
+import subprocess
+
 # check if the library folder already exists, to avoid building everytime you load the pahe
 if not os.path.isdir("/tmp/ta-lib"):
 
@@ -181,21 +181,19 @@ if not os.path.isdir("/tmp/ta-lib"):
     # untar
     os.system("tar -zxvf ta-lib-0.4.0-src.tar.gz")
     os.chdir("/tmp/ta-lib")
+    os.system("ls -la /app/equity/")
     # build
     os.system("./configure --prefix=/home/appuser")
     os.system("make")
     # install
     os.system("make install")
-    # install python package
-    os.system(
-        'pip3 install --global-option=build_ext --global-option="-L/home/appuser/lib/" --global-option="-I/home/appuser/include/" ta-lib'
-    )
     # back to the cwd
     os.chdir(default_cwd)
-    print(os.getcwd())
     sys.stdout.flush()
 
 # add the library to our current environment
+from ctypes import *
+
 lib = CDLL("/home/appuser/lib/libta_lib.so.0.0.0")
 # import library
 try:
